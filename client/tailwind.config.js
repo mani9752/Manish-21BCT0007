a498d1f6722b9ffd7d4234c71909f6e4ad59ceac
mani9752 <139700449+mani9752@users.noreply.github.com>
2024-08-27 00:45:14 +0530

    tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#f9f9f9",
+        card: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
